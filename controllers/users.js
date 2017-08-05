24eebd99ba90ed2f1b46a20b3cb78f5b09d78071
David Gardner <dpgardner10@gmail.com>
2017-08-05 09:25:31 -0700

    created form and connected

--- controllers/users.js
+++ controllers/users.js
@@ -5,10 +5,12 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.render('users/index.ejs');
 })
+//http://localhost:3000/users
 
 //connect route to users/new.ejs
 router.get('/new', (req, res) => {
-  res.render('authors/new.ejs')
+  res.render('users/new.ejs')
 })
+//http://localhost:3000/users/new
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
-
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:false}))
 
 //connect users.js
 const usersController = require('./controllers/users.js')
