4c40c3539455d15243abedcf173bda4a0a76d309
David Gardner <dpgardner10@gmail.com>
2017-08-05 09:18:36 -0700

    setup routes to ejs files

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const router = express.Router();
-//code goes here
+
+//connect route to users/index.ejs
+router.get('/', (req, res) => {
+  res.render('users/index.ejs');
+})
+
+//connect route to users/new.ejs
+router.get('/new', (req, res) => {
+  res.render('authors/new.ejs')
+})
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express();
 
+
+//connect users.js
+const usersController = require('./controllers/users.js')
+app.use('/users', usersController);
+
 //connect homepage route
 app.get('/', (req, res)=>{
   res.render('index.ejs');
