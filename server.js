d1702cce4fd6eab991b1bbdcf90ecb91751f3248
David Gardner <dpgardner10@gmail.com>
2017-08-05 09:00:34 -0700

    created homepage

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express();
-//code here
+
+//connect homepage route
+app.get('/', (req, res)=>{
+  res.render('index.ejs');
+  });
+
 app.listen(3000, () => {
   console.log('listening..')
 })
